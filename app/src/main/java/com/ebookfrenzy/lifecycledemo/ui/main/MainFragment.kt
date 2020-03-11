e39d0f4fee1ac94eeb7d3d2c01c78fff2d4a0a8e
skrall3119 <alexjanci@yahoo.com>
2020-03-11 15:43:09 -0500

    init

---
+++ app/src/main/java/com/ebookfrenzy/lifecycledemo/ui/main/MainFragment.kt
@@ -0,0 +1,40 @@
+package com.ebookfrenzy.lifecycledemo.ui.main
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ebookfrenzy.lifecycledemo.R
+
+import com.ebookfrenzy.lifecycledemo.DemoOwner
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+    private lateinit var demoOwner: DemoOwner
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+
+        demoOwner = DemoOwner()
+        demoOwner.startOwner()
+        demoOwner.stopOwner()
+
+
+    }
+
+}
